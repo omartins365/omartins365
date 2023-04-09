a0083b0d16c3979459e614ef48746dc868a23e87
“Ayoola <omartins365@gmail.com>
2023-04-09 18:05:18 +0100

    update

--- git-commit.sh
+++ git-commit.sh
@@ -1,5 +1,5 @@
 node index.js
 
 git add README.md
-git commit -m"Auto update"
+git commit -m "Auto update"
 
--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,6 @@
 node index.js
 git add README.md
-git commit -m"Auto update"
+git commit -m "Auto update"
 if [ -n "$(git status - porcelain)" ];
 then
  git status
